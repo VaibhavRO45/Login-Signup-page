73939b1d93482112c1012a02d5eb0eb21353f382
VaibhavRO45 <136798271+VaibhavRO45@users.noreply.github.com>
2025-04-22 22:45:41 +0530

    Update info.py

--- gfg/info.py
+++ gfg/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'vaibhavgupta.star002@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehtl tgmz adfi rqju'
+EMAIL_HOST_PASSWORD = 'Enter your password'
 EMAIL_PORT = 587
